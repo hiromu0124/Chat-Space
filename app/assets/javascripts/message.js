57bf6e52ef1c0dc6e60454dc4718839b25724672
hiromu0124 <totohiro0124@icloud.com>
2019-12-03 18:18:33 +0900

    変更しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,43 +1,35 @@
 $(function(){
   function buildHTML(message){
     if (message.image) {
-      document.message_form.reset();
-      var html = `<div class="messages">
-      　　<div class="messages__box">
-      　　　　<div class="messages__box__user">
-      　　　　${message.user_name}
-      　　　　　<div class="messages__box__date">
-      　　　　　　${message.created_at}
-      　　　　　　<div class="messages__box__text">
-      　　　　　　　<div class="messages__text"></div>
-      　　　　　　　　${message.content}</div>
-      　　　　　</div>
-      　　　　</div>
-      　　　　　　　　</div>
-      <img src=${message.image}>
-      </div>`
-    } else {
-      var html = `<div class="messages">
-      　　<div class="messages__box">
-      　　　<div class="messages__box__user">
-      　　　　${message.user_name}
-      　　　　　<div class="messages__box__date">
-      　　　　　　${message.created_at}
-      　　　　　　<div class="messages__box__text">
-      　　　　　　　<div class="messages__text"></div>
-      　　　　　　　　${message.content}</div>
-      　　　　　</div>
-      　　　</div>
-      　　</div>
-      　　　　　　　　</div>`
+      var str=message.image
+    }else{
+      var str=""
     }
+        var html = `<div class="messages">
+      <div class="messages__box">
+      <div class="messages__box__user">
+      ${message.user_name}
+      <div class="messages__box__date">
+      ${message.created_at}
+      <div class="messages__box__text">
+      <div class="messages__text"></div>
+      ${message.content}
+      </div>
+      </div>
+      </div>
+      </div>
+      <img src=${str}>
+      </div>`
     return html
   }
+    
+
   function scroll(){
     $('.messeges').animate({scrollTop: $('.messeges')[0].scrollHeight}, 'fast');
   }
   $("#new_message").on("submit",function(e){
     e.preventDefault();
+
     var formData = new FormData(this);
     var url =$(this).attr("action");
     $.ajax({
